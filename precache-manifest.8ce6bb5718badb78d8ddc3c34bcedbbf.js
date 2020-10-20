35de6915c2fba8888a3953f3658e167effc62468
ratchagarn <ratchagarn@gmail.com>
2020-10-20 20:03:07 +0700

    Updates

---
+++ precache-manifest.8ce6bb5718badb78d8ddc3c34bcedbbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a892f5fe9c9366d33da9d443593fcd10",
+    "url": "/hatohub-export-data/index.html"
+  },
+  {
+    "revision": "98acab5b9a375db6b50c",
+    "url": "/hatohub-export-data/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "f37ff2e5b7cb7dfd7118",
+    "url": "/hatohub-export-data/static/css/main.35f924fe.chunk.css"
+  },
+  {
+    "revision": "98acab5b9a375db6b50c",
+    "url": "/hatohub-export-data/static/js/2.e30a2025.chunk.js"
+  },
+  {
+    "revision": "26bff243f9b75fda4f85add63f8d8d82",
+    "url": "/hatohub-export-data/static/js/2.e30a2025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f37ff2e5b7cb7dfd7118",
+    "url": "/hatohub-export-data/static/js/main.07f5b6fc.chunk.js"
+  },
+  {
+    "revision": "2459e0b1e017bfabadc6",
+    "url": "/hatohub-export-data/static/js/runtime-main.bb4b868e.js"
+  }
+]);
